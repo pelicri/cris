e3496b30c01f4949fb764a4f8e43871360a1fa34
Cristiano <cristianopelizzari@gmail.com>
2020-02-03 21:20:34 -0300

    novo

--- jotinha.js
+++ jotinha.js
@@ -35,15 +35,6 @@ app.get('/',function(req,res){
 	res.end();
 });
 
-app.get('/jota',function(req,res){
-
-	var body = '{ "nome": "Jotinha"}';
-	res.write(body);
-	res.end();
-	
-});
-
-
-app.listen(8080,function(){
+app.listen(80,function(){
 	console.log("Conectado e escutando na porta 8080");
 });
